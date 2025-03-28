2eef6e676f715d208a1ccad0692e111e89b1eefb
Amjad Khan <khan.amjad36@gmail.com>
2025-03-28 21:55:43 +0530

    Initial Commit

---
+++ CatFacts/CatFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatFactsApp.swift
+//  CatFacts
+//
+//  Created by Amjad on 28/03/25.
+//
+
+import SwiftUI
+
+@main
+struct CatFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
